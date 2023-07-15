b6889bcdaa034f6e9172bbb8366c60301d7d6723
Sukhjit Singh <sukhjitsehra@gmail.com>
2023-07-15 00:40:17 -0400

    Updates

---
+++ hello.py
@@ -0,0 +1,48 @@
+
+def hello(a, b, c):
+    """
+    -------------------------------------------------------
+    description: _Type here, what this function will do._
+    Use: _Type here, how we can call this function_
+    -------------------------------------------------------
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+        c (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return var
+    
+
+
+# {{! Custom Docstring Template }}
+
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  your name
+ID:      your id
+Email:   your email
+__updated__ = "${isodate}"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use: 
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
